1601b5d0ef4daef9be7851c7a7137484305065b0
venkatbandike <89034450+venkatbandike@users.noreply.github.com>
2021-08-23 20:01:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+a=5
+b=6
+sum=a+b
+diff=a-b
+mul=a*b
+div=a/b
+mod=a%b
+console.log("The sum of ",a ,"+" ,b ,"is " , sum)
+console.log("The diff of ",a ,"-" ,b ,"is " , diff)
+console.log("The mul of ",a ,"*" ,b ,"is " , mul)
+console.log("The div of ",a ,"/" ,b ,"is " , div)
+console.log("The mod of ",a ,"%" ,b ,"is " , mod)
